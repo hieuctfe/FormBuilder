62e9cedb44cd43abb031a66dda6c6dc028d72d57
LW11995 <=>
2020-08-22 10:44:04 +0700

    tab form and builder

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="hello">
-    <div id="builder"></div>
-    <button  @click="test">Test</button>
+    <button @click="formBuilder">Form builder (create JSON)</button>
+    <button @click="jsonToForm">JSON to form</button>
+    <div style="margin-top: 50px" v-show="tab == 'builder'">
+      <div id="builder"></div>
+      <textarea
+        style="display: block; width: 100%"
+        rows="6"
+        v-model="jsonDataModel"
+      />
+    </div>
+    <div style="margin-top: 50px" v-show="tab == 'form'">
+      <div id="form"></div>
+      <textarea
+        style="display: block; width: 100%"
+        rows="6"
+        v-model="jsonDataModel"
+      />
+    </div>
+    <!-- <button @click="test">Test</button> -->
   </div>
 </template>
 
@@ -15,21 +32,94 @@ export default {
   },
   data() {
     return {
-      testdata: {"components":[{"label":"Select Boxes","labelPosition":"left-right","optionsLabelPosition":"right","tableView":false,"defaultValue":{"":false,"adád":false,"hieu":false,"en":false},"values":[{"label":"hieu","value":"hieu","shortcut":"N"},{"label":"en","value":"en","shortcut":"E"}],"key":"selectBoxes","properties":{"selectBoxkey2":"Hieu"},"type":"selectboxes","input":true,"inputType":"checkbox"},{"label":"Text Area","autoExpand":false,"tableView":true,"key":"textArea","conditional":{"show":true,"when":"selectBoxes","eq":"en"},"type":"textarea","input":true},{"label":"Submit","action":"url","showValidations":false,"size":"sm","block":true,"shortcut":"B","disableOnInvalid":true,"tableView":false,"key":"submit","properties":{"submitkey1":"valuekey1"},"type":"button","url":"http://hieuctfe.github.io/test","input":true,"headers":[{"header":"","value":""}]}]}
-    }
+      dataModel: {
+        components: [
+          {
+            label: "Select Boxes",
+            labelPosition: "left-right",
+            optionsLabelPosition: "right",
+            tableView: false,
+            defaultValue: { "": false, adád: false, hieu: false, en: false },
+            values: [
+              { label: "hieu", value: "hieu", shortcut: "N" },
+              { label: "en", value: "en", shortcut: "E" },
+            ],
+            key: "selectBoxes",
+            properties: { selectBoxkey2: "Hieu" },
+            type: "selectboxes",
+            input: true,
+            inputType: "checkbox",
+          },
+          {
+            label: "Text Area",
+            autoExpand: false,
+            tableView: true,
+            key: "textArea",
+            conditional: { show: true, when: "selectBoxes", eq: "en" },
+            type: "textarea",
+            input: true,
+          },
+          {
+            label: "Submit",
+            action: "url",
+            showValidations: false,
+            size: "sm",
+            block: true,
+            shortcut: "B",
+            disableOnInvalid: true,
+            tableView: false,
+            key: "submit",
+            properties: { submitkey1: "valuekey1" },
+            type: "button",
+            url: "http://hieuctfe.github.io/test",
+            input: true,
+            headers: [{ header: "", value: "" }],
+          },
+        ],
+      },
+      tab: "builder",
+    };
+  },
+  computed: {
+    jsonDataModel: {
+      get: function() {
+        return JSON.stringify(this.dataModel) || "";
+      },
+      // setter
+      set: function(newValue) {
+        this.dataModel = JSON.parse(newValue);
+      },
+    },
   },
   mounted() {
-    // this.builder = Formio.builder(document.getElementById("builder"), this.testdata, {});
-    Formio.createForm(document.getElementById('builder'), this.testdata, {});
-    
+    this.formBuilder();
   },
   methods: {
-    test() {
-      this.builder.then(data => {
-        console.log(JSON.stringify(data.schema))
-      })
-    }
-  }
+    test() {},
+    jsonToForm() {
+      this.tab = "form";
+      this.$nextTick(() => {
+        this.form = Formio.createForm(
+          document.getElementById("form"),
+          this.dataModel,
+          {}
+        );
+      });
+    },
+    formBuilder() {
+      this.tab = "builder";
+      this.builder = Formio.builder(
+        document.getElementById("builder"),
+        this.dataModel,
+        {}
+      );
+    },
+    generateJson() {
+      this.builder.then((data) => {
+        console.log(JSON.stringify(data.schema));
+      });
+    },
+  },
 };
 </script>
 
